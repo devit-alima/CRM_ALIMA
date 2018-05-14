10e97fe387be120974663ddd835fef42292de4eb
Aliou Samba WELE <devit2@alima-ngo.local>
2018-05-14 16:41:22 +0000

    correction bug

--- crm_alima/__manifest__.py
+++ crm_alima/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'crm donateur',
-    'description':'CRM ALIMA'
+    'description':'CRM ALIMA',
     'version': '1.0',
     'category': 'CRM',
     'depends': [
